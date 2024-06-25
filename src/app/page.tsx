29d4201d81b0fbb5538f6781c9fe8d64a22a4f20
Bluefalcon04v <amitsah04v@gmail.com>
2024-06-25 11:24:46 +0530

    Fixing Layout

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Components</h1>
+    <div className="border grid grid-cols-4">
+      <Link href={""}></Link>
+    </div>
+    </>
+  );
+}
