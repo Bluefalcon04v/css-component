658fd46e776f377e45975956e742cb8ff02fe5c2
Amit Sah <amitsah04v@gmail.com>
2024-07-07 00:03:16 +0530

    Styled the Navbar

--- src/components/global/title.tsx
+++ src/components/global/title.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export const Title = ({title}:any) => {
-  return (
-    <div className={`text-3xl font-semibold my-4 `}>
-        {title}
-    </div>
-  )
+export const Title = ({ title }: any) => {
+    return (
+        <div className={`text-3xl font-semibold my-4 `}>
+            {title}
+        </div>
+    )
+}
+
+
+export const BtnTitle = ({ btnTitle }: any) => {
+    return (
+        <p className='text-sm px-4 py-2'>
+            {btnTitle}
+        </p>
+    )
 }
