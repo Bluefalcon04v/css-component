623c1780461eee6fd84e21dee2cfb22cda38b54b
Amit Sah <amitsah04v@gmail.com>
2024-07-18 08:24:06 +0530

    Adding multiple components - Added the text style 1,2,3

---
+++ src/components/TextStyling.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const TextStyling = ({ type }: any) => {
+    if (type === 'text1') return (
+        <div className='text-container'>
+            <div id='textS1' className=' textS'>Text Style</div>
+        </div>
+    )
+
+    if (type === 'text2') return (
+        <div className='text-container'>
+            <div id='textS2' className='textS'>Text Style</div>
+        </div>
+    )
+    if (type === 'text3') return (
+        <div className='text-container overflow-hidden'>
+            <div id='textS3' className='textS bg-[#f7ca05] '>Text Style</div>
+        </div>
+    )
+}
