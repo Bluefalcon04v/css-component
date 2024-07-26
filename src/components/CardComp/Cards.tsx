312a6f1223701bccd3d7101844e9e74ab4dddb86
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2024-07-27 00:45:20 +0530

    wip

--- src/components/CardComp/Cards.tsx
+++ src/components/CardComp/Cards.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const CardComp = ({ type }: any) => {
   if (type === "cardComp1"){
-    return <div>Cards</div>;
+    return (
+      <div id="card-comp1" className="card-comp-container" >
+          Multicolor Border card
+      </div>
+    )
   } 
 };
 
--- src/components/listing/CardsList.tsx
+++ src/components/listing/CardsList.tsx
@@ -4,7 +4,7 @@ import CardComp from "../CardComp/Cards";
 
 export const CardsList = () => {
   return (
-    <div className="mx-auto px-8 py-8 w-11/12 glass-container overflow-hidden">
+    <div className="mx-auto px-8 py-8 w-11/12 glass-container ">
       <Title title="Cards Style" />
       <div className="flex flex-wrap w-full gap-4 ">
         <CardComp type="cardComp1" />
