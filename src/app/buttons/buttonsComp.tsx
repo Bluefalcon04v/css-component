56324a07541d937f7d2f78d1eda1cb5073d02f99
Amit Sah <amitsah04v@gmail.com>
2024-11-18 19:21:17 +0530

    added the button 5

--- src/app/buttons/buttonsComp.tsx
+++ src/app/buttons/buttonsComp.tsx
@@ -57,10 +57,33 @@ export const ButtonsComp = ({ btnTitle, type }: any) => {
         >
           <div className="place-content-center flex items-center">
             <p>Hover Me!</p>
-            <Right_arrow_svg id="button4-svg" className="fill-red-50"/>
+            <Right_arrow_svg id="button4-svg" className="fill-red-50" />
           </div>
         </div>
         <BtnTitle btnTitle={btnTitle} />
       </div>
     );
+
+  if (type === "button5")
+    return (
+        <div className="bg-white w-fit pr-4 rounded-full h-10 flex place-items-center gap-1 cursor-pointer">
+          <div className="w-12 h-12 bg-[#4174A8] rounded-full flex place-content-center place-items-center border-4 border-white">
+            <svg width="18" height="18" viewBox="0 0 24 24">
+              <defs>
+                <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                  <stop offset="0%" stopColor="#fff" stopOpacity="1" />
+                  <stop offset="100%" stopColor="#8DACCA" stopOpacity="1" />
+                </linearGradient>
+              </defs>
+              <path
+                d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"
+                fill="url(#gradient)"
+              />
+            </svg>
+          </div>
+          <div className="text-xl font-black text-[#4174A8] ">
+            Feedback
+          </div>
+        </div>
+      )
 };
